6c834cd23faeb83db99f535cfb6dbfd8ac1981d1
mrgreenh <carloandreaconte@icloud.com>
2020-06-14 20:49:33 -0700

    fix ssr build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,12 +3,12 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-
+const React = require("react");
 const FlowingWrapper = require('./src/components/flowingWrapper').default
 const PageWrapper = require('./src/components/pageWrapper').default
 
 
 // Wraps every page in a component
-exports.wrapRootElement = ({ element, props }) => <FlowingWrapper>{element}</FlowingWrapper>;
+exports.wrapRootElement = ({ element, props }) => <FlowingWrapper  {...props}>{element}</FlowingWrapper>;
 
 exports.wrapPageElement = ({element, props}) => <PageWrapper {...props}>{element}</PageWrapper>;
