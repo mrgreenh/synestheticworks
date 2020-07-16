dbd0bbffe17caf494c5419b560bb59f8fe32d54b
mrgreenh <carloandreaconte@icloud.com>
2020-07-16 12:36:33 -0700

    remove favicon

--- src/pages/synesthesia1.js
+++ src/pages/synesthesia1.js
@@ -22,9 +22,10 @@ const SecondPage = () => (
         height="100%"
         />
     </div>
-    <p>This is the first of what I hope will be many similar projects. I “hope” because then you know, life happens, or laziness, or carpal tunnel etc... So I’m thankful already I got the bandwidth and peace of mind necessary to work on this first one.
-The idea is: augment the experience of producing music, by adding a compelling visual dimension to it. Such visual dimension is supposed to be engaging and fully expressive (not having to compromise too much in terms of visual quality/realism/abstraction) and is enabled by the data typically available to electronic music producers (MIDI + audio + automations).
-This first project itself had the objective of paving a workflow that would make it possible to produce this kind of content single-handedly, on a regular basis (2-3 months cycles?), thus maintaining the personal nature of “bedroom music production”.</p>
+    <p>This is the first of what I hope will be many similar projects.<br/>
+    I “hope” because then you know, life happens, or laziness, or carpal tunnel etc... So I’m thankful already I could get the bandwidth and peace of mind necessary to work on this first one.</p>
+    <p>The idea is: augment the experience of producing music, by adding a compelling visual dimension to it. Such visual dimension is supposed to be engaging and fully expressive (not having to compromise too much in terms of visual quality/realism/abstraction) and is enabled by the data typically available to electronic music producers (MIDI + audio + automations).<br/>
+    This first project itself had the objective of paving a workflow that would make it possible to produce this kind of content single-handedly, on a regular basis (2-3 months cycles?), thus maintaining the personal nature of “bedroom music production”.</p>
   </Layout>
 )
 
