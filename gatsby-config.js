0b260e5d2b07dcb435505e93f746fc4ea725697a
mrgreenh <carloandreaconte@icloud.com>
2020-07-16 12:12:54 -0700

    pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/synestehticworks",
   siteMetadata: {
     title: `Synesthetic Works`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
