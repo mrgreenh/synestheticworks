05d94c09c0ee572bd3e7d9a38cbf6f7634109845
mrgreenh <carloandreaconte@icloud.com>
2020-08-23 14:07:47 -0700

    updating syn2 page

--- src/pages/abluedwarftale.js
+++ src/pages/abluedwarftale.js
@@ -19,6 +19,21 @@ const SecondPage = () => {
           }
         }
       },
+      asteroids: file(relativePath: { eq: "asteroid-field-cropped.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1080) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+      replicator: file(relativePath: { eq: "replicator.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1080) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+
     }
   `)
 
@@ -26,7 +41,13 @@ const SecondPage = () => {
     <BlogHeader ogimage="/asteroids.png" title="A Blue Dwarf's Tale [Synesthesia #2]" date="August 11, 2020" />
     <i>A mysterious, far away star will pull you through the parallel universe where music and space-time are entangled.</i>
     <p>This project is landing here soon. Follow on <a href="https://bit.ly/synwrks-facebook" target="_blank">Facebook</a>, <a href="https://bit.ly/synwrks-twitter" target="_blank">Twitter</a> or <a href="https://bit.ly/synwrks-instagram" target="_blank">Instagram</a> to stay tuned!</p>
+    <div className="laurels">
+      <img src={"/laurels/indiex.png"}/>
+      <img src={"/laurels/indieshorts.png"}/>
+    </div>
     <Img className="movie-poster" fluid={data.syn2poster.childImageSharp.fluid}/>
+    <Img className={"movie-screenshot"} fluid={data.replicator.childImageSharp.fluid}/>
+    <Img className={"movie-screenshot"} fluid={data.asteroids.childImageSharp.fluid}/>
     
   </Layout>;
 }
