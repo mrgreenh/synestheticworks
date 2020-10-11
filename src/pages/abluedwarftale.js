dfd33d1a31b96757b7c94fb957edd41ff29aea56
mrgreenh <carloandreaconte@icloud.com>
2020-10-11 15:41:08 -0700

    add links to VR syn2

--- src/pages/abluedwarftale.js
+++ src/pages/abluedwarftale.js
@@ -41,14 +41,7 @@ const SecondPage = () => {
 
   return <Layout>
     <BlogHeader ogimage="/asteroids.png" title="A Blue Dwarf's Tale [Synesthesia #2]" date="August 11, 2020" />
-    <div className="laurels">
-      <img src={"/laurels/indiex.png"}/>
-      <img src={"/laurels/californiainternationalshorts.png"}/>
-      <img src={"/laurels/indieshorts.png"}/>
-      <div className=".separator"/>
-      <img src={"/laurels/sanfranindependentshorts.png"}/>
-      <img src={"/laurels/socal.png"}/>
-    </div>
+    <p><i>A mysterious, far away star will pull you through the parallel universe where music and space-time are entangled.</i></p>
     <div className='player-wrapper'>
       <div className="placeholder">
         Loading Player...
@@ -61,8 +54,23 @@ const SecondPage = () => {
         height="100%"
         />
     </div>    
-    <i>A mysterious, far away star will pull you through the parallel universe where music and space-time are entangled.</i>
-    <p>The VR version of this video will also be added to this page soon :)</p>
+    <div className="laurels">
+      <img src={"/laurels/indiex.png"}/>
+      <img src={"/laurels/californiainternationalshorts.png"}/>
+      <img src={"/laurels/indieshorts.png"}/>
+      <div className=".separator"/>
+      <img src={"/laurels/sanfranindependentshorts.png"}/>
+      <img src={"/laurels/socal.png"}/>
+    </div>
+    <p><b>This looks even better in VR!</b> If you have a headset, there are a couple ways to watch:
+    <ul>
+      <li>Youtube: <a href="https://bit.ly/2GZUtJa" title="Youtube VR video" target="_blank">save this video</a> to a playlist, then find it in your headset on the Youtube VR app!</li>
+      <li>Oculus TV: <a href="https://bit.ly/synesthesia2" title="Oculus VR video" target="_blank">save this video</a>, then find it in your Oculus headset (Go, Quest) on the Oculus TV app!</li>
+    </ul>
+    </p>
+    <p>
+      This is a 180 3D video, so don't forget to look around. Use headphones if you can ;) 
+    </p>
     <p>Check out the <a target="_blank" href="https://bit.ly/synwrks-soundcloud">SynestheticWorks Soundcloud</a> where you can find the original soundtrack, or the <a target="_blank" href="https://bit.ly/synwrks-instagram">Instragram</a> and <a target="_blank" href="https://bit.ly/synwrks-facebook">Facebook</a> pages where you can find updates, additional artwork, and perhaps more synesthesias.</p>
     <i>- Carlo</i>
     {/* <Img className={"movie-screenshot"} fluid={data.replicator.childImageSharp.fluid}/>
