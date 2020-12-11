d298c68dad678fa8f94a8bfe961a319cdc0c35a8
mrgreenh <carloandreaconte@icloud.com>
2020-12-10 22:11:24 -0800

    added instructions for visual quality in vr

--- src/pages/abluedwarftale.js
+++ src/pages/abluedwarftale.js
@@ -7,6 +7,7 @@ import BlogHeader from "../components/blogHeader"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import ReactPlayer from "react-player"
+import { AnchorLink } from "gatsby-plugin-anchor-links"
 
 import "./abluedwarfstale.scss"
 import "./player.scss"
@@ -14,34 +15,21 @@ import "./player.scss"
 const SecondPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      syn2poster: file(relativePath: { eq: "syn2_poster.png" }) {
+      otv_syn2_instructions: file(relativePath: { eq: "otv_syn2_instructions_ink_cropped.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 1080) {
-            ...GatsbyImageSharpFluid
+          fixed(width: 800) {
+            ...GatsbyImageSharpFixed
           }
         }
       },
-      asteroids: file(relativePath: { eq: "asteroid-field-cropped.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1080) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      },
-      replicator: file(relativePath: { eq: "replicator.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1080) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      },
-
     }
   `)
 
   return <Layout>
     <BlogHeader ogimage="/asteroids.png" title="A Blue Dwarf's Tale [Synesthesia #2]" date="August 11, 2020" />
-    <p><i>A mysterious, far away star will pull you through the parallel universe where music and space-time are entangled.</i></p>
+    <p><i>A mysterious, far away star will pull you through the parallel universe where music and space-time are entangled.</i><br/>
+    [Jump to: <AnchorLink to="/abluedwarftale#vr_instructions" title="Instructions for VR viewing">How to view in VR</AnchorLink>]
+    </p>
     <div className='player-wrapper'>
       <div className="placeholder">
         Loading Player...
@@ -62,10 +50,16 @@ const SecondPage = () => {
       <img src={"/laurels/sanfranindependentshorts.png"}/>
       <img src={"/laurels/socal.png"}/>
     </div>
+    <a name="vr_instructions"/>
+    <h3>How to view in VR</h3>
     <p><b>This looks even better in VR!</b> If you have a headset, there are a couple ways to watch:
     <ul>
-      <li>Youtube: <a href="https://bit.ly/2GZUtJa" title="Youtube VR video" target="_blank">save this video</a> to a playlist, then find it in your headset on the Youtube VR app!</li>
-      <li>Oculus TV: <a href="https://bit.ly/synesthesia2" title="Oculus VR video" target="_blank">save this video</a>, then find it in your Oculus headset (Go, Quest) on the Oculus TV app!</li>
+      <li>Youtube (best audio-video sync): <a href="https://bit.ly/2GZUtJa" title="Youtube VR video" target="_blank">save this video</a> to a playlist, then find it in your headset on the Youtube VR app!</li>
+      <li>Oculus TV (best visual quality): <a href="https://bit.ly/synesthesia2" title="Oculus VR video" target="_blank">save this video</a>, then find it in your Oculus headset (Go, Quest) > Oculus TV > Saved</li>
+      <ul>
+        <li>For the <b>best visual quality</b>, when you find the video in the headset, tap on the 3 dots and select "Cache High Quality". Allow time for the download > enjoy!</li>
+        <li><Img fixed={data.otv_syn2_instructions.childImageSharp.fixed}/></li>
+      </ul>
     </ul>
     </p>
     <p>
@@ -73,9 +67,6 @@ const SecondPage = () => {
     </p>
     <p>Check out the <a target="_blank" href="https://bit.ly/synwrks-soundcloud">SynestheticWorks Soundcloud</a> where you can find the original soundtrack, or the <a target="_blank" href="https://bit.ly/synwrks-instagram">Instragram</a> and <a target="_blank" href="https://bit.ly/synwrks-facebook">Facebook</a> pages where you can find updates, additional artwork, and perhaps more synesthesias.</p>
     <i>- Carlo</i>
-    {/* <Img className={"movie-screenshot"} fluid={data.replicator.childImageSharp.fluid}/>
-    <Img className={"movie-screenshot"} fluid={data.asteroids.childImageSharp.fluid}/>
-    <Img className="movie-poster" fluid={data.syn2poster.childImageSharp.fluid}/> */}
   </Layout>;
 }
 
