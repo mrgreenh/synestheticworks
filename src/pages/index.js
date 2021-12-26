b30a1464ad511647d761d79421f8015da32c5788
mrgreenh <carloandreaconte@icloud.com>
2021-12-26 13:31:20 -0700

    blog contents page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { useStaticQuery, graphql } from "gatsby"
 
 import "./index.scss"
 import ShortsMosaic from "../components/mosaics/ShortsMosaic"
 import MixesMosaic from "../components/mosaics/MixesMosaic"
 import NFTsMosaic from "../components/mosaics/NFTsMosaic"
+import BlogContents from "../components/blogContents"
 
-const IndexPage = () => {
-  
-  const data = useStaticQuery(graphql`
-  query {
-    allMarkdownRemark (sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          frontmatter {
-            title
-            slug
-            date(formatString: "MMMM DD, YYYY")
-            summary
-          }
-        }
-      }
-    }
-  }
-  `)
-  
+const IndexPage = () => {  
   return (
     <Layout>
         <SEO title={"Home"} image={"/home.png"}/>
@@ -38,18 +20,8 @@ const IndexPage = () => {
         <MixesMosaic/>
         {/* <h2>NFTs</h2>
         <NFTsMosaic/> */}
-        <div className="posts-list">
-          {data.allMarkdownRemark.edges.map(edge => 
-          <ul key={edge.node.frontmatter.slug}>
-            <li>
-              <Link to={edge.node.frontmatter.slug}>
-                <h2>{edge.node.frontmatter.title}</h2>
-                <h4>{edge.node.frontmatter.date}</h4>
-                <p>{edge.node.frontmatter.summary}</p>
-              </Link>
-            </li>
-          </ul>)}
-        </div>
+        <h2>Blog</h2>
+        <BlogContents count={2} includeDescriptions={false}/>
     </Layout>
   )
 }
