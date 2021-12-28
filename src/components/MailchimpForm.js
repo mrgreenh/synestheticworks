bb20f050793184d62192a94c87aa00b2b52d55b6
mrgreenh <carloandreaconte@icloud.com>
2021-12-28 09:41:37 -0700

    make the mailchimp form more resilient

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -4,7 +4,9 @@ import addToMailchimp from 'gatsby-plugin-mailchimp';
 export default class SubscribeForm extends React.Component {
   state = {
     email: '',
-    message: ''
+    message: '',
+    showPlanB: false,
+    loading: false
   };
 
   handleInputChange = (event) => {
@@ -18,14 +20,26 @@ export default class SubscribeForm extends React.Component {
 
   handleSubmit = async (e) => {
     e.preventDefault();
+    this.setState({showPlanB: false});
+    setTimeout(() => {
+      this.setState({showPlanB: true, loading: false});
+    }, 2000);
+    this.setState({loading: true, message: ''})
     const result = await addToMailchimp(this.state.email);
-    this.setState({ message: result.msg });
+    this.setState({ message: result.msg, loading: false });
   };
 
   render() {
     return (
       <form name="subscribeForm" method="POST" id="subscribe-form" className="subscribe-form" onSubmit={this.handleSubmit}>
-        <div className="message" dangerouslySetInnerHTML={{ __html: this.state.message}} />
+        {!this.state.loading && <div className="message" dangerouslySetInnerHTML={{ __html: this.state.message}} />}
+        {this.state.loading && <div className="message">Loading...</div>}
+        {!!this.state.showPlanB 
+          && !this.state.message.length 
+          && <div className="message">
+              Seems like something is not working. Try subscribe from the <a href="http://eepurl.com/hQ3rAn" target="_blank">
+                Mailchimp page directly</a>. Sorryyy :'(
+            </div>}
         <div className="form-row">
           <label>
             <span className="screen-reader-text">Keep up with rare updates:</span>
