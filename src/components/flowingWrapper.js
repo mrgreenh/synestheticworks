278f0cbfc6ffaa123cec9f7e50ed95a6f98a4b1f
mrgreenh <carloandreaconte@icloud.com>
2022-01-01 22:18:27 -0800

    GDPR

--- src/components/flowingWrapper.js
+++ src/components/flowingWrapper.js
@@ -6,6 +6,8 @@ import ReadingTracker from "./FlowReadingTracker"
 import TilesFlowContext from "./utils/TilesFlowContext"
 import MailchimpForm from "./MailchimpForm"
 import {Link} from 'gatsby'
+import CookieConsent from 'react-cookie-consent';
+import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies'
 
 import "./generics.scss"
 
@@ -30,6 +32,25 @@ const FlowingWrapper = ({ children }) => {
             </div>
           </div>
         </div>
+        <CookieConsent
+          location="bottom"
+          buttonText="Accept"
+          declineButtonText="No"
+          style={{backgroundColor: "#333340", color: "white", fontSize: "0.8rem", paddingTop: 0}}
+          contentStyle={{marginTop: 0, marginBottom: 0}}
+          buttonStyle={{backgroundColor: "#696", color: "white", margin: 0}}
+          enableDeclineButton
+          declineButtonStyle={{backgroundColor: "#444450"}}
+          flipButtons={true}
+          onAccept={() => {   
+            initializeAndTrack("/")
+          }}
+          cookieName="gatsby-gdpr-google-analytics">
+            Hi! With your consent, this site will use Google Analytics (which uses cookies) to provide the site's author with anonymized statistics about the audience of the website. <a 
+              style={{color: "white"}} 
+              href="https://policies.google.com/technologies/partner-sites" 
+              target="_blank">Google's Privacy and Terms</a>
+        </CookieConsent>
       </TilesFlowContext.Provider>
     </>
   )
